ef245febecfe2942c3e23f658a9b049ff1d0ba46
Pontus Österdahl <85453963+pontus-osterdahl@users.noreply.github.com>
2023-03-16 16:25:09 +0100

    Added missing import

--- Kitodo/src/main/java/org/kitodo/production/helper/ProcessHelper.java
+++ Kitodo/src/main/java/org/kitodo/production/helper/ProcessHelper.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
