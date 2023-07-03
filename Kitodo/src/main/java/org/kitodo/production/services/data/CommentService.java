7f7039fa56eb492784d18bc042acc51e80c05371
Pontus Österdahl <85453963+pontus-osterdahl@users.noreply.github.com>
2023-07-03 15:31:20 +0200

    Add removal of comment

--- Kitodo/src/main/java/org/kitodo/production/services/data/CommentService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/CommentService.java
@@ -17,10 +17,16 @@ import java.util.Objects;
 
 import org.kitodo.data.database.beans.Comment;
 import org.kitodo.data.database.beans.Process;
+import org.kitodo.data.database.beans.Task;
+import org.kitodo.data.database.beans.User;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.database.persistence.CommentDAO;
+import org.kitodo.production.services.ServiceManager;
 import org.kitodo.production.services.data.base.SearchDatabaseService;
 import org.primefaces.model.SortOrder;
+import java.io.IOException;
+
+import org.kitodo.data.exceptions.DataException;
 
 public class CommentService extends SearchDatabaseService<Comment, CommentDAO> {
 
@@ -78,5 +84,19 @@ public class CommentService extends SearchDatabaseService<Comment, CommentDAO> {
      */
     public void saveList(List<Comment> list) throws DAOException {
         dao.saveList(list);
+    }
+    
+    /**
+     * Remove comment from database and resolve associations.
+     * 
+     * @param comment to be removed.
+     */
+    public static void removeComment(Comment comment) throws DAOException {
+        comment.getProcess().getComments().remove(comment);
+        comment.setProcess(null);
+        comment.setAuthor(null);
+        comment.setCorrectionTask(null);
+        comment.setCurrentTask(null);
+        ServiceManager.getCommentService().removeFromDatabase(comment);
     }
 }
